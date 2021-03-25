dcb39a6b6338c9f180c82af95a290a08b18a6293
Miguel Leite <amlmiguel@gmail.com>
2021-03-25 15:19:47 -0300

    atividade proposta do dia 25/03

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ let pets = [{
     tutor: 'João',
     contato: '(81) 99999-9999',
     vacinado: true,
-    servicos: ['banho','tosa']
+    servicos: ['tosa','Aparar unha']
 },
 {
     nome: 'Ajudante de papai noel',
@@ -30,7 +30,7 @@ let pets = [{
     peso: 15,
     tutor: 'Homer',
     contato: '(81) 99999-9999',
-    vacinado: true,
+    vacinado: false,
     servicos: ['banho']
 }];
 
@@ -42,5 +42,113 @@ const listarPets = () => {
     }
 }
 
-listarPets();
+// listarPets();
 // console.log(pet);
+
+const vacinarPet = () => {
+    for(let pet of pets){
+        if(pet.vacinado == false){
+            pet.vacinado = true;
+            console.log(`O pet ${pet.nome} foi vacinado com sucesso !`);
+        }else{
+            console.log(`Ops, ${pet.nome} já foi vacinado`)
+        }
+        
+    }
+}
+
+// vacinarPet();
+
+const CampanhaVacina = () => {
+    var soma = 0;
+    for(let pet of pets){
+        if (pet.vacinado == false){
+            soma++;
+        }
+    }
+    console.log(`${soma} foram vacinados!`)
+}
+
+console.log("\n")
+
+// CampanhaVacina();
+
+const adicionarPet = (nome, tipo,idade,raca,peso,tutor,contato,vacinado,servicos) => {
+    pets.push({
+        nome: nome,
+        tipo: tipo,
+        idade: idade,
+        raca: raca,
+        peso: peso,
+        tutor: tutor,
+        contato: contato,
+        vacinado: vacinado,
+        servicos: servicos
+    });
+}
+
+adicionarPet('Brian','Cachorro',15,'Beagle',12,'Peter','9999-9999',true,['tosa']);
+
+for(let pet of pets){
+    console.log(`${pet.nome} recebeu os seguintes cuidados: ${pet.servicos}`)
+}
+
+const DarBanhoPet = () => {
+    for(let pet of pets){
+        for(let servico of pet.servicos){
+            if(!(pet.servicos.includes('banho')
+            )){
+            pet.servicos.push('banho');
+            }
+        }
+        
+    }
+ }
+        
+ 
+
+DarBanhoPet();
+
+
+const TosarPet = () => {
+    for(let pet of pets){
+        for(let servico of pet.servicos){
+            if(!(pet.servicos.includes('tosa')
+            )){
+            pet.servicos.push('tosa');
+            }
+        }
+        
+    }
+ }
+        
+ 
+
+TosarPet();
+
+const ApararUnha = () => {
+    for(let pet of pets){
+        for(let servico of pet.servicos){
+            if(!(pet.servicos.includes('Aparar unha')
+            )){
+            pet.servicos.push('Aparar unha');
+            }
+        }
+        
+    }
+ }
+        
+ 
+
+ApararUnha();
+
+console.log('\n')
+
+console.log("Após as aplicação das funções...")
+
+console.log('\n')
+for(let pet of pets){
+    console.log(`${pet.nome} recebeu os seguintes cuidados: ${pet.servicos}`)
+}
+
+ 
